9ebff0761a392dee2e4ea11ecbbc652ee4592cc5
feilerr <feilerr@126.com>
2016-09-23 17:21:25 +0800

    1.0.5
    修正MainActivity不需要的代码

--- app/src/main/java/com/example/feilerr/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/feilerr/androiddemo/MainActivity.java
@@ -12,10 +12,6 @@ import com.example.viewFactory.LayoutFactory;
 import com.example.viewFactory.MEditText;
 import com.example.viewFactory.MTextView;
 import com.example.viewFactory.MView;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import zframework.base.BaseActivity;
 import zframework.global.GlobalValue;
@@ -23,11 +19,6 @@ import zframework.support.ShareData;
 
 public class MainActivity extends BaseActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,44 +57,6 @@ public class MainActivity extends BaseActivity {
         userLayout.layout.addView(et);
 
         Log.i(GlobalValue.logTag, "onCreate");
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
